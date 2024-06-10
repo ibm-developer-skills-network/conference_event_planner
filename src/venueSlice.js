27932dd4d458824bb3c196e2c212119893b79d5d
RichaArora1989 <richaar@skillup.tech>
2024-06-11 00:08:16 +0530

    images link

--- src/venueSlice.js
+++ src/venueSlice.js
@@ -5,31 +5,31 @@ export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "",
+      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://pixabay.com/images/download/convention-center-3908238_640.jpg",
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
     },
     {
-      img: "",
+      img: "https://pixabay.com/images/download/laptops-593296_640.jpg",
       name: "Small Meeting Room (Capacity:5)",
       cost: 1100,
       quantity: 0,
